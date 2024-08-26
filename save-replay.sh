abcf71292e0368e521bc068a731f89f581605af6
YourBoyRory <yourboyrory@gmail.com>
2024-08-26 00:09:22 -0400

    fixed issue where script detection never worked if the GUI was open

--- save-replay.sh
+++ save-replay.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=/opt/gpu-screen-recorder-scripts/splash.sh
 
-killall -SIGUSR1 gpu-screen-recorder
+killall -e -SIGUSR1 gpu-screen-recorder
 exitcode=$?
 if [[ $exitcode -ne 0 ]]; then
     $SCRIPT_DIR fail
